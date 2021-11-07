65207291cd3ea5901f8719ad556dd2edb04e76c2
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2021-11-07 03:24:58 +0100

    Upload failed test results to imgur

--- pytest_mpl/plugin.py
+++ pytest_mpl/plugin.py
@@ -32,6 +32,7 @@ import io
 import os
 import glob
 import json
+import base64
 import shutil
 import hashlib
 import logging
@@ -58,6 +59,26 @@ SHAPE_MISMATCH_ERROR = """Error: Image dimensions did not match.
 RASTER_IMAGE_FORMATS = ['png']
 VECTOR_IMAGE_FORMATS = ['eps', 'pdf', 'svg']
 ALL_IMAGE_FORMATS = RASTER_IMAGE_FORMATS + VECTOR_IMAGE_FORMATS
+
+
+def _upload_to_imgur(filename):
+    try:
+        import requests
+    except ImportError:
+        return "requests module missing, no image upload possible"
+
+    with open(filename, 'rb') as image_file:
+        img_data = base64.b64encode(image_file.read())
+
+    headers = {'Authorization': 'Client-ID 24d952aae51d11d'}
+    data = {'type': 'base64', 'name': os.path.basename(filename), 'image': img_data}
+
+    try:
+        r = requests.post("https://api.imgur.com/3/image", data=data, headers=headers)
+        r.raise_for_status()
+        return r.json()['data']['link']
+    except requests.exceptions.RequestException as e:
+        return "Image upload failed: {0}".format(e)
 
 
 def _hash_file(in_stream):
@@ -149,6 +170,8 @@ def pytest_addoption(parser):
                     ", in --mpl-results-path. The type of the report should be "
                     "specified. Supported types are `html`, `json` and `basic-html`. "
                     "Multiple types can be specified separated by commas.")
+    group.addoption('--mpl-upload', action='store_true',
+                    help="Enable uploading of failed test images to imgur.")
 
     results_path_help = "directory for test results, relative to location where py.test is run"
     group.addoption('--mpl-results-path', help=results_path_help, action='store')
@@ -587,6 +610,9 @@ class ImageComparison:
                     cur_summ['rms'] = results['rms']
                     cur_summ['diff_image'] = Path(results['diff']).relative_to(self.results_dir).as_posix()
                     cur_summ['status_msg'] = error_message
+
+        if self.config.getoption("--mpl-upload"):
+            all_msgs += "Test image: " + _upload_to_imgur(test_image) + "\n\n"
 
         summary.update(cur_summ)
         return all_msgs.strip()
